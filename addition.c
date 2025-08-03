6dc78d8b1819f58ea99a80ea20f0e22f076ecf89
NopeThisIsSoumya <pachhalsoumyasri@gmail.com>
2025-08-03 22:19:09 +0530

    sum of two number

---
+++ addition.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int number1;
+    int number2;
+    printf("Enter two numbers");
+    scanf("%d %d",&number1, &number2);
+    printf("sum of two number %d",number1+number2);
+}
