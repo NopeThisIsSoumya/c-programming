97631a7ab4ef224082fab751aeae61db2f2aa4dd
NopeThisIsSoumya <pachhalsoumyasri@gmail.com>
2025-08-04 22:55:32 +0530

    addition division substraction multiplkicatin

--- addition.c
+++ addition.c
@@ -4,5 +4,9 @@ int main(){
     int number2;
     printf("Enter two numbers");
     scanf("%d %d",&number1, &number2);
-    printf("sum of two number %d",number1+number2);
+    printf("sum of two number %d \n",number1+number2);
+    printf("substraction of two number %d \n" ,number1-number2 );
+    printf("multiplication of tywo number %d \n" , number1*number2);
+    printf("division of two number %d",number1/number2);
+    
 }
