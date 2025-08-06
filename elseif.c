1f98459900fb5f94ad4e748ae013063fa6e9aa4d
NopeThisIsSoumya <pachhalsoumyasri@gmail.com>
2025-08-06 21:47:13 +0530

    voter age limit

--- elseif.c
+++ elseif.c
@@ -5,16 +5,19 @@ int main(){
 
     scanf("%d",&age);
     printf("you have entered %d as your age\n" , age);
+    
      if(age>=18){
         printf("you can vote");
 
      }
      else if(age>=12){
         printf("you can vote for kids");
+
      }
 
      else{
         printf("you can not vote");
+
 
      }
     
