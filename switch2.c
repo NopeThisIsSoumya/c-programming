c316f5230c1126cbfc54176c547fdc09fdd9d48d
NopeThisIsSoumya <pachhalsoumyasri@gmail.com>
2025-08-09 21:22:55 +0530

    day name by just entering day no

---
+++ switch2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+
+    int day;
+    printf("enter day no\n");
+    scanf("%d", &day);
+    
+    switch(day) 
+    {
+        case 1:
+        printf("today is monday\n" );
+        break;
+
+        case 2:
+        printf("today is tuesday\n");
+        break;
+
+        case 3:
+        printf("today is wednesday\n");
+        break;
+
+        case 4:
+        printf("today is thrusday\n");
+        break;
+        case 5:
+        printf("today is friday\n");
+        break;
+
+        case 6:
+        printf("today is saturday\n");
+        break;
+
+        case 7:
+        printf("today is sunday\n");
+        break;
+
+        default:
+        printf("you entered wrong day no");
+
+    }
+
+}
