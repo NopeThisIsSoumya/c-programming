71bdb1789339ed60b6e4f579c3cb3c8741ac3142
NopeThisIsSoumya <pachhalsoumyasri@gmail.com>
2025-08-21 23:38:27 +0530

    function example

---
+++ function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int sum(int a , int b)
+{
+    return a+b;
+
+}
+int main()
+{
+    int a , b , c;
+    printf ("enter two number\n");
+    scanf("%d %d " , &a, &b);
+     c = sum(a,b);
+     printf("the sum is %d \n" , c);
+
+     return 0;
+
+}
